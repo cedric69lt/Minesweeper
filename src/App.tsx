8a669324c9bd8904b2047d75407e04d35ff60f52
UnEpicier <alexisvasseur96@gmail.com>
2024-02-12 19:22:03 +0100

    ✨ (Game): Help popup
    Help popup to learn how to play

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 //!                                                       Imports
 // ---------------------------------------------------------------------------------------------------------------------
 
+// ------------------------------------------------------ React --------------------------------------------------------
+import { useState } from 'react';
+// ---------------------------------------------------------------------------------------------------------------------
+
 // ----------------------------------------------------- Context -------------------------------------------------------
 import { RecoilRoot } from 'recoil';
 // ---------------------------------------------------------------------------------------------------------------------
@@ -10,14 +14,18 @@ import { RecoilRoot } from 'recoil';
 import Grid from './components/Grid/Grid';
 import Timer from './components/Stats/Timer/Timer';
 import MinesCounter from './components/Stats/MinesCounter/MinesCounter';
+import Help from './components/Help/Help';
 // ---------------------------------------------------------------------------------------------------------------------
 
 // ----------------------------------------------------- Styles --------------------------------------------------------
-import './App.scss';
 import Logo from './assets/logo';
+import './App.scss';
+import Question from './assets/question';
 // ---------------------------------------------------------------------------------------------------------------------
 
 const App = () => {
+	const [helpDisplayed, displayHelp] = useState(false);
+
 	return (
 		<div className='App'>
 			<RecoilRoot>
@@ -27,7 +35,21 @@ const App = () => {
 					<MinesCounter />
 				</div>
 				<Grid />
+				<button
+					className='helpButton'
+					onClick={() => displayHelp(true)}
+				>
+					<Question />
+					<span>Comment jouer</span>
+				</button>
 			</RecoilRoot>
+			{helpDisplayed && (
+				<Help
+					onClose={() => {
+						displayHelp(false);
+					}}
+				/>
+			)}
 		</div>
 	);
 };
---
+++ src/components/Help/Help.tsx
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------------------------------------------------------------
+//!                                                       Imports
+// ---------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------- Assets --------------------------------------------------------
+import Close from '../../assets/close';
+import './styles.scss';
+// ---------------------------------------------------------------------------------------------------------------------
+
+const Help = ({ onClose }: { onClose: () => void }) => {
+	return (
+		<div className='helpContainer'>
+			<div className='helpPopup'>
+				<div className='header'>
+					<p className='title'>Comment jouer ?</p>
+					<button
+						className='closeButton'
+						onClick={onClose}
+					>
+						<Close />
+					</button>
+				</div>
+				<div className='content'>
+					<p>
+						Pour commencer, il suffit de cliquer n'importe où sur la grille.
+						<br />
+						Les cases vides vont alors se dévoiler jusqu'à atteindre les cases contenant des numéros.
+						<br />
+						<br />
+						Chaque numéro indique que dans les 8 cases autour de celle-ci, il y a X bombes (numéro affiché).
+						<br />
+						Le but est de dévoiler toute la grille sans faire exploser une bombe en cliquant dessus.
+						<br />
+						<br />
+						Pour éviter tout accident, vous pouvez poser un drapeau sur les cases où vous pensez qu'il y a une bombe en faisant clic droit. Le clic sera alors désactivé jusqu'à ce que vous retiriez le drapeau.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Help;
